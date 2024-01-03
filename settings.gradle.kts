b8dc5d7e64e86e8fca369ac7d1d68bc8423b54a9
KartikNagrale <kartikknagrale@gmail.com>
2024-01-03 22:43:07 +0530

    30 days of android devlopment

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "The 30 Days of Android Development"
+include(":app")
